2624800430fed092517f1c7839320915b38a035a
HarshDubey1011 <pandetahallu@gmail.com>
2026-06-08 21:59:07 +0530

    added

---
+++ src/main/java/com/hd/SemanticInterview/SemanticInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.hd.SemanticInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hd/SemanticInterview/SemanticInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hd.SemanticInterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
